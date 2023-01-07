794402d6971ab79236cdc8c984eb5c2f1b89a17f
NewUser <hammadmalli64@gmail.com>
2023-01-07 10:32:01 +0500

    setting up project

--- lib/services/dio_client.dart
+++ lib/services/dio_client.dart
@@ -1,10 +1,27 @@
 import 'package:dio/dio.dart';
-import 'package:git_repo/services/exceptions.dart';
+
+import 'end_points.dart';
 
 class DioClient{
-  BaseOptions options = new BaseOptions(
-    baseUrl: Endpoints,
-    headers: ,
-    connectTimeout: ,
+  static BaseOptions options = new BaseOptions(
+    // baseUrl: Endpoints.baseUrl,
+    // headers: ,
+    // connectTimeout: ,
   );
+
+  Dio _dio = Dio(options);
+
+  Future<dynamic> get(String uri,
+      {
+        Map<String, dynamic>? queryparams,
+        Options? options,
+        CancelToken? cancelToken,
+        ProgressCallback? onRecieneProgress,
+  })async{
+    try{
+      final
+    }catch(e){
+      rethrow;
+    }
+  }
 }
