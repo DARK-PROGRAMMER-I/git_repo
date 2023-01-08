d649c3485be12e6731242ea795df82c15f0d02f2
NewUser <hammadmalli64@gmail.com>
2023-01-08 22:33:27 +0500

    setting up project

--- lib/presentation/user/user_screens/user_detail_screen.dart
+++ lib/presentation/user/user_screens/user_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../common/common_libs.dart';
@@ -8,15 +9,59 @@ class UserDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 80.h,),
-          Row(
-            children: [
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Column(
+          children: [
+            SizedBox(height: 80.h,),
+            Row(
+              children: [
+                Container(
+                  width: 80.w,
+                  height: 80.h,
+                  color: MyColors.black,
+                  child: CachedNetworkImage(
+                    imageUrl: "",
+                    imageBuilder: (context, imageProvider) => Container(
+                      width: 120.w,
+                      height: 120.h,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: imageProvider, fit: BoxFit.fitHeight),
+                      ),
+                    ),
+                    placeholder: (context, url) => SizedBox(
+                        width: 92.w,
+                        height: 75.h,
+                        child: Center(
+                            child: SizedBox(
+                                width: 30.w,
+                                height: 30.h,
+                                child: CircularProgressIndicator()))),
+                    errorWidget: (context, url, error) => SizedBox(
+                        width: 92.w,
+                        height: 75.h,
+                        child: Center(
+                            child: SizedBox(
+                              width: 30.w,
+                              height: 30.h,
+                              child: Icon(Icons.error),))),
+                  ),
+                ),
+                SizedBox(
+                  width: 10.w,
+                ),
+                Column(
+                  children: [
 
-            ],
-          )
-        ],
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
